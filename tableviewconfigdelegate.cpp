23d8b8541108231d21932678335b04cc7fa6bce7
b.ulyashev <ikar.chek@gmail.com>
2021-12-24 20:24:27 +0300

    Add class delegate for tableViewConfig

---
+++ tableviewconfigdelegate.cpp
@@ -0,0 +1,6 @@
+#include "tableviewconfigdelegate.h"
+
+TableViewConfigDelegate::TableViewConfigDelegate()
+{
+
+}
---
+++ tableviewconfigdelegate.h
@@ -0,0 +1,13 @@
+#ifndef TABLEVIEWCONFIGDELEGATE_H
+#define TABLEVIEWCONFIGDELEGATE_H
+
+#include <QItemDelegate>
+
+class TableViewConfigDelegate : public QItemDelegate
+{
+    Q_OBJECT
+public:
+    TableViewConfigDelegate();
+};
+
+#endif // TABLEVIEWCONFIGDELEGATE_H
